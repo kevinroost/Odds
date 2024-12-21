e2112769b5222977f9c259fa71310f695a5912b8
KevinRoost <kroost92@gmail.com>
2024-12-21 18:12:08 -0500

    add ballybet and bet us to default bms

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   const [events, setEvents] = useState([])
   const [targetEvents, setTargetEvents] = useState([])
   const bms = ['betonlineag', 'betmgm', 'betrivers', 'betus', 'bovada', 'draftkings', 'fanduel', 'lowvig', 'mybookieag', 'ballybet']
-  const [desiredBms, setDesiredBms] = useState(['draftkings', 'fanduel', 'betrivers'])
+  const [desiredBms, setDesiredBms] = useState(['draftkings', 'fanduel', 'betrivers', 'ballybet', 'betus'])
   const [finalString, setFinalString] = useState([])
   const [predictions, setPredictions] = useState({status: 999, resArr: []})
   const [today, setToday] = useState(true)
